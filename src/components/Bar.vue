f1c007a47ff97dcb954f3a237a5c4f8494023708
snow.jhung <snowwayne1231@gmail.com>
2021-02-09 14:39:31 +0800

    save for char room

--- src/components/Bar.vue
+++ src/components/Bar.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home bg-img" id="rv-bar">
-    <md-card class="home-card">
+    <md-card class="home-card bar-card">
       <md-card-header>
         <md-card-header-text>
           <div class="md-title">
@@ -14,7 +14,23 @@
       </md-card-header>
 
       <md-card-content>
-        
+        <div class="bar-content">
+
+        </div>
+        <div class="chat-box">
+          <div class="chat-history" ref="chatHistory">
+            <ul>
+              <li v-for="(history, idx) in chat.histories" :key="idx">
+                <label>{{history.nickname}}</label>
+                <span>: {{history.text}}</span>
+              </li>
+            </ul>
+          </div>
+          <div class="chat-tools">
+            <input class="chat-tool-input" type="text" v-model="chatInput" maxlength="48" v-on:keyup.enter="onInputEnter" />
+            <button @click="sendMessage">Submit</button>
+          </div>
+        </div>
       </md-card-content>
     </md-card>
   </div>
@@ -23,7 +39,7 @@
 <script>
 import { mapState } from 'vuex';
 import RadarChart from './chart/radar';
-import { ACT_JOIN_CHAT_ROOM } from '../store/enum';
+import { ACT_JOIN_CHAT_ROOM, ACT_LEAVE_CHAT_ROOM, ACT_JOIN_FAMILY, ACT_SAY_CHAT_ROOM } from '../store/enum';
 
 export default {
   name: 'Bar',
@@ -32,24 +48,35 @@ export default {
   },
   data() {
     return {
-      
-    }
+      chatInput: '',
+    };
   },
   mounted() {
     // console.log(this);
-    this.sendMessage();
+    this.$store.dispatch('wsEmitMessage', {act: ACT_JOIN_CHAT_ROOM, payload: {}});
   },
   beforeDestroy() {
-
+    this.$store.dispatch('wsEmitMessage', {act: ACT_LEAVE_CHAT_ROOM, payload: {}});
   },
   computed: {
-    ...mapState(['user']),
+    ...mapState(['user', 'chat']),
+  },
+  updated() {
+    if (this.$refs['chatHistory']) {
+      this.$refs['chatHistory'].scrollTop = this.$refs['chatHistory'].scrollHeight;
+    }
   },
   methods: {
     sendMessage(evt) {
-      console.log('Bar sendMessage');
-      this.$store.dispatch('wsEmitMessage', {act: ACT_JOIN_CHAT_ROOM, payload: {}});
-    }
+      // console.log('Bar sendMessage');
+      if (this.chatInput.length > 0) {
+        this.$store.dispatch('wsEmitMessage', {act: ACT_SAY_CHAT_ROOM, payload: {text: this.chatInput.substr(0,47)}});
+        this.chatInput = '';
+      }
+    },
+    onInputEnter(evt) {
+      this.sendMessage(evt);
+    },
   }
 }
 </script>
