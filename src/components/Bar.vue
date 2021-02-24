2d3f9c6d6a5e32af2cd4f98e5b417c4760eacbea
snow.jhung <snowwayne1231@gmail.com>
2021-02-24 14:08:02 +0800

    v1.0.3 hotfix bar table

--- src/components/Bar.vue
+++ src/components/Bar.vue
@@ -93,7 +93,7 @@ export default {
     showWantedPeople: {
       get() {
         return this.chat.publicPeople.filter(e => {
-          return e.houseId == 0 && e.houseIdTmp == 0 && e.mvp == 0 && e.isLeader == false;
+          return e.houseId == 0 && e.houseIdTmp == 0 && e.mvp == 0 && e.isLeader == false && e.nickname;
         });
       },
       set(next) {
